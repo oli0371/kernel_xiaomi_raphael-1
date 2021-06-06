kdrag0n/proton_zf6

drm/msm/dsi-staging: Add display timing override cmdline parameter
Useful for providing a non-overclocked display option to users.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -64,6 +64,9 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{}
 };
 
+static unsigned int timing_override;
+module_param(timing_override, uint, 0444);
+
 //ASUS BSP Display features
 #define ASUS_LCD_REGISTER_RW     "driver/panel_reg_rw"
 #define ASUS_CABC_PROC_FILE      "driver/cabc"
@@ -6699,6 +6702,11 @@ int dsi_display_get_modes(struct dsi_display *display,
 	num_dfps_rates = !dfps_caps.dfps_support ? 1 : dfps_caps.dfps_list_len;
 
 	panel_mode_count = display->panel->num_timing_nodes;
+	if (timing_override >= panel_mode_count) {
+		pr_warn("[%s] ignoring invalid cmdline timing override %d\n",
+			display->name, timing_override);
+		timing_override = 0;
+	}
 
 	for (mode_idx = 0; mode_idx < panel_mode_count; mode_idx++) {
 		struct dsi_display_mode panel_mode;
@@ -6706,6 +6714,9 @@ int dsi_display_get_modes(struct dsi_display *display,
 
 		if (display->cmdline_timing == mode_idx)
 			topology_override = display->cmdline_topology;
+
+		if (mode_idx != timing_override)
+			continue;
 
 		memset(&panel_mode, 0, sizeof(panel_mode));
 

oli0371/kernel_xiaomi_raphael-1

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -62,6 +62,9 @@ static const struct of_device_id dsi_display_dt_match[] = {
 
 struct dsi_display *primary_display;
 
+static unsigned int timing_override;
+module_param(timing_override, uint, 0444);
+
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -6475,6 +6478,11 @@ int dsi_display_get_modes(struct dsi_display *display,
 	dyn_clk_caps = &(display->panel->dyn_clk_caps);
 
 	timing_mode_count = display->panel->num_timing_nodes;
+	if (timing_override >= timing_mode_count) {
+		pr_warn("[%s] ignoring invalid cmdline timing override %d\n",
+			display->name, timing_override);
+		timing_override = 0;
+	}
 
 	for (mode_idx = 0; mode_idx < timing_mode_count; mode_idx++) {
 		struct dsi_display_mode display_mode;
@@ -6482,6 +6490,9 @@ int dsi_display_get_modes(struct dsi_display *display,
 
 		if (display->cmdline_timing == mode_idx)
 			topology_override = display->cmdline_topology;
+
+		if (mode_idx != timing_override)
+			continue;
 
 		memset(&display_mode, 0, sizeof(display_mode));
 