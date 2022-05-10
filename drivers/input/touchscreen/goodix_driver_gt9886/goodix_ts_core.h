6ab90355d3a9d37268425db82d2a16fa73265027
kondors1995 <normandija1945@gmail.com>
2022-05-10 13:43:37 +0000

    Revert "drivers: input: touchscreen: fix leftover goodix lockdown code"
    This reverts commit 1fae66cf8415b62e96020929a8aabc8076dd6eb2.

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.h
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_core.h
@@ -461,6 +461,7 @@ struct goodix_ts_core {
 	unsigned long sleep_finger;
 	unsigned long event_status;
 	unsigned int avdd_load;
+	unsigned char lockdown_info[GOODIX_LOCKDOWN_SIZE];
 
 	int power_on;
 	int irq;
